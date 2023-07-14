d42a120b5a32c873f313c0ad904215d171759ac4
emily <eli.mhs24@gmail.com>
2023-07-14 13:51:11 -0500

    Initial Commit

---
+++ toDoListApp/toDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListAppApp.swift
+//  toDoListApp
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
